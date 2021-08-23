b588653348ca36d39761866100e6f4f3b9c2076e
Katilyn Wiggins <katilynswiggins@gmail.com>
2021-08-23 13:09:10 +0000

    add signup form to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+    else 
+      render 'new' 
+    end
+  end
+  
+  private 
+    #defining strong parameters
+    def user_params
+      params.require(:user).permit(:name, :emal, :password, :password_confirmation)
+    end
 end
+
